bba080331ea7c6de04e1304dc62d42ca8a53eeae
EliverLara <eliverlara@gmail.com>
2020-03-14 11:45:08 -0600

    Update xfce-panel appearance

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,11 +3,11 @@ var sass = require('gulp-sass');
 var exec = require('gulp-exec');
 
 gulp.task('styles', function(done) {
-    gulp.src('gtk-3.20/**/*.scss')
+    gulp.src('gtk-3.0/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./gtk-3.20/'))
+        .pipe(gulp.dest('./gtk-3.0/'))
         .pipe(exec(' gsettings set org.gnome.desktop.interface gtk-theme "Nordic"'))
-        .pipe(exec(' gsettings set org.gnome.desktop.interface gtk-theme "stelio"'))
+        .pipe(exec(' gsettings set org.gnome.desktop.interface gtk-theme "Kripton"'))
     done();
 });
 
@@ -15,13 +15,13 @@ gulp.task('shell-style', function(done) {
     gulp.src('gnome-shell/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./gnome-shell/'))
-        .pipe(exec('gsettings set org.gnome.shell.extensions.user-theme name "stelio"'))
+        .pipe(exec('gsettings set org.gnome.shell.extensions.user-theme name "Kripton"'))
     done();
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('gtk-3.20/**/*.scss', gulp.series('styles'));
+    gulp.watch('gtk-3.0/**/*.scss', gulp.series('styles'));
 });
 
 gulp.task('shell',function() {
