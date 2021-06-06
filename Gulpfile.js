7be519bfce5a0b207bf087ad1d7865eeeb0a1048
EliverLara <eliverlara@gmail.com>
2021-06-06 13:48:43 -0500

    Gnome: Add initial support to v40

--- Gulpfile.js
+++ Gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('styles', function(done) {
 });
 
 gulp.task('shell-style', function(done) {
-    gulp.src('gnome-shell/*.scss')
+    gulp.src('gnome-shell/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./gnome-shell/'))
         .pipe(exec('gsettings set org.gnome.shell.extensions.user-theme name "Kripton"'))
@@ -25,5 +25,5 @@ gulp.task('default',function() {
 });
 
 gulp.task('shell',function() {
-    gulp.watch('gnome-shell/*.scss', gulp.series('shell-style'));
+    gulp.watch('gnome-shell/**/*.scss', gulp.series('shell-style'));
 });
